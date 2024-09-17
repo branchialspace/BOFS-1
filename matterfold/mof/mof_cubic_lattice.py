c643c86ad49fb78faa9b2243f5cb162b414d36ea
George Tsatas <31433587+branchialspace@users.noreply.github.com>
2024-09-17 15:07:00 -0400

    save mof

--- matterfold/mof/mof_cubic_lattice.py
+++ matterfold/mof/mof_cubic_lattice.py
@@ -7,9 +7,7 @@ from ase.io import write
 from ase.build import make_supercell
 
 
-# Arranges two molecules in a cubic lattice using ASE
 def cubic_lattice_mof(num_bismuth: int, ligand: str, lattice_constant=10.0, repetitions=(3, 3, 3)):
-
     metal_unit = generate_bismuth_polyhedron(num_bismuth)
     ligand_unit = generate_ligand(ligand)
     
@@ -26,5 +24,11 @@ def cubic_lattice_mof(num_bismuth: int, ligand: str, lattice_constant=10.0, repe
          [0, 0, repetitions[2]]]
     
     mof = make_supercell(cell, P)
+    
+    metal_name = f"Bi{num_bismuth}"
+    ligand_name = "".join(ligand_unit.get_chemical_symbols())
+    repetitions_str = f"{repetitions[0]}x{repetitions[1]}x{repetitions[2]}"
+    filename = f"{metal_name}_{ligand_name}_MOF_{repetitions_str}.xyz"
+    write(filename, mof)
     
     return mof
