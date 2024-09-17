3954469874c973350e286fe8b8937e3d76081c85
George Tsatas <31433587+branchialspace@users.noreply.github.com>
2024-09-17 14:59:01 -0400

    Update cubic_lattice_mof.py

--- matterfold/mof/cubic_lattice_mof.py
+++ matterfold/mof/cubic_lattice_mof.py
@@ -5,62 +5,12 @@ from rdkit.Chem import AllChem
 from ase import Atoms
 from ase.io import write
 from ase.build import make_supercell
-from ase.optimize import BFGS
-from ase.calculators.lj import LennardJones
 
-
-# SMILES to RDKit to ASE Atoms object
-def generate_ligand(smiles: str) -> Atoms:
-    mol = Chem.MolFromSmiles(smiles)
-    mol = Chem.AddHs(mol)
-    AllChem.EmbedMolecule(mol, AllChem.ETKDG())
-    AllChem.UFFOptimizeMolecule(mol)
-    
-    conformer = mol.GetConformer()
-    atoms = []
-    positions = []
-    
-    for atom in mol.GetAtoms():
-        symbol = atom.GetSymbol()
-        atoms.append(symbol)
-    
-    for i in range(mol.GetNumAtoms()):
-        pos = conformer.GetAtomPosition(i)
-        positions.append([pos.x, pos.y, pos.z])
-    
-    atoms_obj = Atoms(atoms, positions=positions)
-
-    filename = "".join(atoms_obj.get_chemical_symbols()) + ".xyz"
-    write(filename, atoms_obj)
-    
-    return atoms_obj
-
-# Octohedral Bismuth Core ASE Atoms object
-def generate_octahedral_bismuth() -> Atoms:
-    positions = [
-        (1, 0, 0),
-        (-1, 0, 0),
-        (0, 1, 0),
-        (0, -1, 0),
-        (0, 0, 1),
-        (0, 0, -1)
-    ]
-    atoms = Atoms('Bi6', positions=positions)
-    
-    lj_calc = LennardJones(sigma=3.5, epsilon=0.2)
-    atoms.calc = lj_calc
-    optimizer = BFGS(atoms)
-    optimizer.run(fmax=0.01)
-
-    filename = 'Bi6_octahedron.xyz'
-    write(filename, atoms)
-    
-    return atoms
 
 # Arranges two molecules in a cubic lattice using ASE
-def cubic_lattice_mof(ligand: str, lattice_constant=10.0, repetitions=(3, 3, 3)):
+def cubic_lattice_mof(num_bismuth: int, ligand: str, lattice_constant=10.0, repetitions=(3, 3, 3)):
 
-    metal_unit = generate_octahedral_bismuth()
+    metal_unit = generate_bismuth_polyhedron(num_bismuth)
     ligand_unit = generate_ligand(ligand)
     
     cell = Atoms(cell=[(lattice_constant, 0, 0), (0, lattice_constant, 0), (0, 0, lattice_constant)])
