ce963034db72941b1b5e897ea09787c519dbb858
George Tsatas <31433587+branchialspace@users.noreply.github.com>
2024-10-14 14:34:01 -0400

    Create electric_steric_alignment.py

---
+++ matterfold/mofs/electric_steric_alignment.py
@@ -0,0 +1,5 @@
+# surface-accessible and bifocal binding-site charge symmetry
+
+# binding site angle = max distance from internal ligand bonds
+
+# steric hinderence in single-cluster octahedral coordination
