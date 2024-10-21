1d2f772c9ac23b3adb40eb873b11369cfe12a00f
George Tsatas <31433587+branchialspace@users.noreply.github.com>
2024-10-21 14:42:42 -0400

    Update electric_steric_alignment.py

--- matterfold/mofs/electric_steric_alignment.py
+++ matterfold/mofs/electric_steric_alignment.py
@@ -1,4 +1,4 @@
 # Bisected bonding-site charge symmetry for 1d bridging ligands
-# steric hindrance in single-cluster octahedral coordination
-    # bonding site angle = max distance from internal ligand bonds
+# Steric hindrance in single-cluster octahedral coordination
+    # Bonding site angle = max distance from internal ligand bonds
 
