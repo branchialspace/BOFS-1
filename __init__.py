36f89bea5d0e6a60c2fd183580625862b55a0a14
George Tsatas <93tsatas@gmail.com>
2024-12-22 11:31:52 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,4 +7,4 @@ from .matterfold.mof_v1.mof_nanoparticle import mof_nanoparticle
 from .matterfold.mof_v1.ligand_metal_docking import ligand_metal_docking
 from .matterfold.mof_v1.orca_docking import orca_docking
 from .matterfold.mof_v1 import utils
-from .matterfold.models.matbench_discovery import soap 
+from .matterfold.models.matbench_discovery.soap import soap 
