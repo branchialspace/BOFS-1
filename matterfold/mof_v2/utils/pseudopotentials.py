29449143ea650e190c5bd56b5cac688030bb0df1
George Tsatas <93tsatas@gmail.com>
2025-01-23 14:58:52 -0500

    add sssp

--- matterfold/mof_v2/utils/pseudopotentials.py
+++ matterfold/mof_v2/utils/pseudopotentials.py
@@ -1,4 +1,4 @@
-# Dalcorso pslibrary
+# Dalcorso pslibrary: fully-relativistic 
 !wget https://people.sissa.it/dalcorso/pslibrary/pslibrary.1.0.0.tar.gz
 !tar -xzvf pslibrary.1.0.0.tar.gz
 !sudo ln -s /content/bin/ld1.x /bin/ld1.x
@@ -6,3 +6,9 @@
 %cd /content/pslibrary.1.0.0
 !chmod +x make_all_ps
 !./make_all_ps
+
+# SSSP library: non-relativistic
+!gdown 1w--QOWnmlPZDy9qJp0NmFakRKHkY8_pt
+!gdown 1FoHw9CJT78LItQuaxvrsmgpJ5nrpK1gp
+!mkdir -p pseudo_sssp
+!tar -xf SSSP_1.3.0_PBE_efficiency.tar.gz -C pseudo_sssp
