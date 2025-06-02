8be9242c97d11cfe99c89dbe80b3278d14670e40
George Tsatas <93tsatas@gmail.com>
2025-06-02 13:17:54 -0400

    Update bofs1_env.sh

--- bofs1/bofs1_env.sh
+++ bofs1/bofs1_env.sh
@@ -4,7 +4,7 @@ set -e # Exit on error
 python3 -m venv bofs1_env
 source bofs1_env/bin/activate
 sudo apt-get update
-sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-pip unzip
 pip install numpy==1.26.4
 pip install torch_geometric
 pip install wandb
@@ -19,7 +19,6 @@ unzip bimofs2.zip -d mofs
 # QuantumESPRESSO
 wget ...
 tar -xzf qe-7.4.1-ReleasePack.tar.gz
-sudo apt-get update
 sudo apt-get install -y liblapack-dev libblas-dev libopenmpi-dev libscalapack-openmpi-dev libfftw3-dev
 cmake -DCMAKE_C_COMPILER=mpicc -DQE_FFTW_VENDOR=Internal -DQE_ENABLE_OPENMP=ON qe-7.4.1
 make -j4
