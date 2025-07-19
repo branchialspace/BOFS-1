10aaf6b0f896e6cba3d7ba509cab4c72c32d83b5
George Tsatas <93tsatas@gmail.com>
2025-07-18 23:50:39 -0400

    use environment variables for sensitive urls

--- bofs1_env.sh
+++ bofs1_env.sh
@@ -13,11 +13,11 @@ pip install ase
 pip install rdkit
 pip install mendeleev
 pip install gdown
-gdown ... # 369 Bi MOFs from ARCMOF, CSDMOF, QMOF, MOSAEC-DB
+gdown $MOF_DB_GDOWN # 369 Bi MOFs from ARCMOF, CSDMOF, QMOF, MOSAEC-DB
 mkdir -p mofs
 unzip bimofs2.zip -d mofs
 # QuantumESPRESSO
-wget ...
+wget $QE_URL
 tar -xzf qe-7.4.1-ReleasePack.tar.gz
 sudo apt-get install -y liblapack-dev libblas-dev libopenmpi-dev libscalapack-openmpi-dev libfftw3-dev
 cmake -DCMAKE_C_COMPILER=mpicc -DQE_FFTW_VENDOR=Internal -DQE_ENABLE_OPENMP=ON qe-7.4.1
@@ -26,7 +26,7 @@ make ld1
 mkdir -p qe-7.4.1/bin
 cp bin/ld1.x qe-7.4.1/bin/
 # Dalcorso fully-relativistic pseudopotentials
-gdown ...
+gdown $DALCORSO_GDOWN
 unzip dalcorso_rel_pbe.zip
 # ONCV fully-relativistic pseudopotentials repositories
 git clone https://github.com/pipidog/ONCVPSP.git
