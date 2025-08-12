ead207b483ebbbfb0e92e0323ce85c7c81e784fd
George Tsatas <93tsatas@gmail.com>
2025-08-12 17:57:53 -0400

    remove bofs1 repo git clone

--- bofs1_env.sh
+++ bofs1_env.sh
@@ -35,8 +35,6 @@ unzip dalcorso_rel_pbe.zip
 # ONCV fully-relativistic pseudopotentials repositories
 git clone https://github.com/pipidog/ONCVPSP.git
 git clone https://github.com/MarioAndWario/ONCVPseudoPack.git
-# BOFS1
-git clone https://github.com/branchialspace/BOFS-1.git
 # BOFS1 venv wrapper
 cat > qe_run << 'EOF'
 #!/bin/bash
