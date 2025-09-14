b78b982c8dee06ea35b64032160387771702924c
George Tsatas <93tsatas@gmail.com>
2025-09-14 15:36:08 -0400

    remove old config import

--- bofs1/__init__.py
+++ bofs1/__init__.py
@@ -20,7 +20,6 @@ from .qe.qe_magnonx import qe_magnonx
 from .qe.qe_phx import qe_phx
 from .qe.qe_projwfcx import qe_projwfcx
 from .qe.qe_run import qe_run
-from .qe.configs import pwx_scf
 
 __all__ = [
     # Assembly functions
