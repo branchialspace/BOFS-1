82b600f6a8190cb68615193b6237d06d77b02bd6
George Tsatas <93tsatas@gmail.com>
2025-09-14 16:23:43 -0400

    remove hard-coded config

--- bofs1/qe/magnonx.py
+++ bofs1/qe/magnonx.py
@@ -72,20 +72,6 @@ def magnonx(
             print(f"Could not read output file: {e}")
     except Exception as e:
         print(f"Unexpected error: {e}")
-
-
-magnonx_config = {
-    'command': ['/usr/bin/mpirun', '--allow-run-as-root', '-x', 'OMP_NUM_THREADS=2', '-np', '4', '/content/bin/turbo_magnon.x'],
-    'q_vector': [0.0, 0.0, 0.0],    # q-vector for magnon calculations in units of 2pi/a
-    'lr_control': {
-        'itermax': 500,             # Number of Lanczos iterations
-        'pseudo_hermitian': True,   # Use pseudo-Hermitian Lanczos algorithm
-        'approximation': 'TDDFT',   # Level of theory: 'TDDFT' or 'IPA'
-        'ipol': 4,                  # Polarization: 1-3 for specific component, 4 for full tensor
-    }
-}
-
-
 mof = "/content/mofs/SIWZOO_full_n2.cif"
 # Run turbo_magnon.x
 qe_magnonx(mof, magnonx_config)
