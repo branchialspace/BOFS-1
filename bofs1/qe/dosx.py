2760bfda190e3524156ce8d246b3e29bcf5ee965
George Tsatas <93tsatas@gmail.com>
2025-09-14 16:22:01 -0400

    remove hard-coded config and direct run

--- bofs1/qe/dosx.py
+++ bofs1/qe/dosx.py
@@ -64,19 +64,3 @@ def dosx(
             print(f"Could not read output file: {e}")
     except Exception as e:
         print(f"Unexpected error: {e}")
-
-
-dosx_config = {
-    'command': ['/usr/bin/mpirun', '--allow-run-as-root', '-x', 'OMP_NUM_THREADS=2', '-np', '4', '/content/bin/dos.x'],
-    'dos': {
-        'bz_sum': 'smearing',        # Method for Brillouin zone summation
-        'ngauss': -99,                 # Type of gaussian broadening (-99 = Fermi-Dirac)
-        'degauss': 0.01,             # Gaussian broadening in Ry
-        'DeltaE': 0.05,              # Energy grid step in eV
-    }
-}
-
-# ASE structure
-mof = "/content/mofs/SIWZOO_full_n2.cif"
-# Run dosx
-qe_dosx(mof, dosx_config)
