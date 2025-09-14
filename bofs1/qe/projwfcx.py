3f2805e4a6abc08208bf5ea489460ae3d3e0fef9
George Tsatas <93tsatas@gmail.com>
2025-09-14 16:25:30 -0400

    remove hard-coded config and direct run

--- bofs1/qe/projwfcx.py
+++ bofs1/qe/projwfcx.py
@@ -65,23 +65,3 @@ def projwfcx(
             print(f"Could not read output file: {e}")
     except Exception as e:
         print(f"Unexpected error: {e}")
-
-
-wfcx_config = {
-    'command': ['/usr/bin/mpirun', '--allow-run-as-root', '-x', 'OMP_NUM_THREADS=2', '-np', '4', '/content/bin/projwfc.x'],
-    'projwfc': {
-        'ngauss': -99,                # Type of gaussian broadening (-99 = Fermi-Dirac)
-        'degauss': 0.01,              # Gaussian broadening in Ry
-        'DeltaE': 0.05,               # Energy grid step in eV
-        'lsym': False,                # Symmetrize projections
-        'diag_basis': False,           # Project onto global or local XYZ frame
-        'pawproj': False,             # Use PAW projectors (for PAW pseudos only)
-        'lwrite_overlaps': False,     # Orbital overlap matrix (for parallel, paste , '-nd', '1' to end of command)
-        'kresolveddos': True,         # Sum over all k-points (not k-resolved)
-    }
-}
-
-# ASE structure
-mof = "/content/mofs/SIWZOO_full_n2.cif"
-# Run projwfc.x
-qe_projwfcx(mof, wfcx_config)
