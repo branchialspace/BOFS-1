b4617cd39751a5466b3c2ca3137f7794add7556f
George Tsatas <93tsatas@gmail.com>
2025-09-20 10:31:28 -0400

    Update paths, PAW

--- bofs1/qe/qe_configs/projwfcx_config.py
+++ bofs1/qe/qe_configs/projwfcx_config.py
@@ -1,12 +1,12 @@
 projwfcx_config = {
-    'command': ['/usr/bin/mpirun', '--allow-run-as-root', '-x', 'OMP_NUM_THREADS=2', '-np', '4', '/content/bin/projwfc.x'],
+    'command': ['./bofs1_env/bin/mpirun', '--allow-run-as-root', '-x', 'OMP_NUM_THREADS=2', '-np', '4', './bin/projwfc.x'],
     'projwfc': {
-        'ngauss': -99,                # Type of gaussian broadening (-99 = Fermi-Dirac)
+        'ngauss': 0,                # Type of gaussian broadening (0 = Gaussian, -99 = Fermi-Dirac)
         'degauss': 0.01,              # Gaussian broadening in Ry
         'DeltaE': 0.05,               # Energy grid step in eV
         'lsym': False,                # Symmetrize projections
         'diag_basis': False,           # Project onto global or local XYZ frame
-        'pawproj': False,             # Use PAW projectors (for PAW pseudos only)
+        'pawproj': True,             # Use PAW projectors (for PAW pseudos only)
         'lwrite_overlaps': False,     # Orbital overlap matrix (for parallel, paste , '-nd', '1' to end of command)
         'kresolveddos': True,         # Sum over all k-points (not k-resolved)
     }
