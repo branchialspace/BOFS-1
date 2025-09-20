e3ca95282c03d21db8e32ed766f908af764533f7
George Tsatas <93tsatas@gmail.com>
2025-09-20 10:55:11 -0400

    pawproj not implemented in noncolinear case

--- bofs1/qe/qe_configs/projwfcx_config.py
+++ bofs1/qe/qe_configs/projwfcx_config.py
@@ -6,7 +6,7 @@ projwfcx_config = {
         'DeltaE': 0.05,               # Energy grid step in eV
         'lsym': False,                # Symmetrize projections
         'diag_basis': False,           # Project onto global or local XYZ frame
-        'pawproj': True,             # Use PAW projectors (for PAW pseudos only)
+        'pawproj': False,             # Use PAW projectors (for PAW pseudos only, not implemented in noncolinear case)
         'lwrite_overlaps': False,     # Orbital overlap matrix (for parallel, paste , '-nd', '1' to end of command)
         'kresolveddos': True,         # Sum over all k-points (not k-resolved)
     }
