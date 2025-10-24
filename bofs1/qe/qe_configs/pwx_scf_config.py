093d6bf57e5b21e68239b725f32361194079cb2c
George Tsatas <93tsatas@gmail.com>
2025-10-24 16:05:33 -0400

    Update pwx_scf_config.py

--- bofs1/qe/qe_configs/pwx_scf_config.py
+++ bofs1/qe/qe_configs/pwx_scf_config.py
@@ -1,5 +1,5 @@
 pwx_scf_config = {
-    'command': ['./bofs1_env/bin/mpirun', '--allow-run-as-root', '-x', 'OMP_NUM_THREADS=2', '-np', '4', './bin/pw.x'],
+    'command': ['./bofs1_env/bin/mpirun', '--allow-run-as-root', '-x', 'OMP_NUM_THREADS=1', '-np', '11', './bin/pw.x'],
     'wfn_scalar': 1.15,
     'rho_scalar': 1.15,
     'kpts_k_spacing': 0.13, # scf: 0.13    nscf: 0.09
