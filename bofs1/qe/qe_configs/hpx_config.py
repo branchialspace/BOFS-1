be238b2f8f5cadcb7432c4c26f4600bee896d03e
George Tsatas <93tsatas@gmail.com>
2025-11-10 19:08:38 -0500

    Modify inputhp parameters in hpx_config.py

--- bofs1/qe/qe_configs/hpx_config.py
+++ bofs1/qe/qe_configs/hpx_config.py
@@ -10,8 +10,8 @@ hpx_config = {
     'qpts_q_spacing': 0.065,        # q-point spacing
     'inputhp': {
         'iverbosity': 2,           # Verbosity level
-        'find_atpert': 4,          # Method for determining which atoms to perturb (4=Perturb all Hubbard atoms)
-        'skip_equivalence_q': True, # If True, use full q-point grid; if False, use symmetry to reduce q-points
+        'find_atpert': 1,          # Method for determining which atoms to perturb (4=Perturb all Hubbard atoms)
+        'skip_equivalence_q': False, # If True, use full q-point grid; if False, use symmetry to reduce q-points
         'num_neigh': 16,            # Number of nearest neighbors for Hubbard V parameters
         'lmin': 0,                 # Minimum orbital quantum number for Hubbard V parameters
         'rmax': 20,             # Maximum distance to search for neighbors
