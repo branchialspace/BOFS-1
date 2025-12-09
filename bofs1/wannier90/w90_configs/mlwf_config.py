70d2754790847cafd09d7e297e81907476ca69f2
George Tsatas <93tsatas@gmail.com>
2025-12-08 19:00:07 -0500

    Add MLWF configuration settings in mlwf_config.py
    Added configuration options for MLWF in wannier90.

--- bofs1/wannier90/w90_configs/mlwf_config.py
+++ bofs1/wannier90/w90_configs/mlwf_config.py
@@ -1,1 +1,25 @@
+mlwf_config = {
+    "spinors": "true",
+    "num_iter": 2000,
+    "dis_num_iter": 1000,
+    "guiding_centres": "true",
+    "write_hr": "true",
+    "write_xyz": "true",
+    "translate_home_cell": "true",
+    "iprint": 2,
+    "dis_win_min_offset": -15.0, 
+    "dis_win_max_offset": 20.0,
+    "dis_froz_min_offset": -15.0,
+    "dis_froz_max_offset": 2.0, 
+    "bands_plot": "true",
+    "wannier_plot": "true",
+    "wannier_plot_format": "xcrysden",
+    "bands_plot_format": "gnuplot",
+    "kpoint_path": [
+        "G 0.0 0.0 0.0  X 0.5 0.0 0.0",
+        "X 0.5 0.0 0.0  M 0.5 0.5 0.0",
+        "M 0.5 0.5 0.0  G 0.0 0.0 0.0",
+        "G 0.0 0.0 0.0  R 0.5 0.5 0.5"
+    ]
+}
 
