81c35bfbda85fc4748d0f73cfae1163ec7263241
George Tsatas <93tsatas@gmail.com>
2025-12-12 16:56:42 -0500

    typo

--- bofs1/wannier90/w90_configs/mlwf_config.py
+++ bofs1/wannier90/w90_configs/mlwf_config.py
@@ -1,14 +1,12 @@
 mlwf_config = {
     "spinors": "true",
-    "num_iter": 2000,
-    "dis_num_iter": 1000,
+    "dis_num_iter": 0,   # for SCDM
+    "num_iter": 0,       # Preserves symmetry for Yambo
     "guiding_centres": "true",
     "write_hr": "true",
     "write_xyz": "true",
     "translate_home_cell": "true",
     "iprint": 2,
-    "dis_num_iter": 0,   # for SCDM
-    "num_iter": 0,       # Preserves symmetry for Yambo
     "bands_plot": "true",
     "wannier_plot": "true",
     "wannier_plot_format": "xcrysden",
