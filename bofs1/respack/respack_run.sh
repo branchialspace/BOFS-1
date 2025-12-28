322dc20060c86bc4d0eeeb17bb46b8489be9ce7d
George Tsatas <93tsatas@gmail.com>
2025-12-28 16:15:03 -0500

    hardcode respack.in file creation, remove inputs already set by wan2respack method

--- bofs1/respack/respack_run.sh
+++ bofs1/respack/respack_run.sh
@@ -45,24 +45,31 @@ wan2respack_post () {
 # Run RESPACK
 respack_run () {
     local calc_dir="$1"
-    local qe_bands_dir="$2"
-    local respack_input="$3"
-    local omp_stacksize="${4:-16}"
-    local omp_num_threads="${5:-16}"
-    local mpi_np="${6:-1}"
+    local omp_stacksize="${2:-16}"
+    local omp_num_threads="${3:-16}"
+    local mpi_np="${4:-1}"
     mkdir -p "$calc_dir"
     conda activate "$ROOT_DIR/bofs1_env_py27"
     export OMP_STACKSIZE="$omp_stacksize"
     export OMP_NUM_THREADS="$omp_num_threads"
-    cp "$ROOT_DIR/RESPACK/util/qe2respack/qe2respack.py" "$calc_dir/"
     cd "$calc_dir" || return 1
-    python ./qe2respack.py "$qe_bands_dir"
-    cp "$respack_input" ./input.in
-    mpirun -np "$mpi_np" ./calc_wannier < input.in > log.wannier
-    mpirun -np "$mpi_np" ./calc_chiqw < input.in > log.chiqw
-    mpirun -np "$mpi_np" ./calc_w3d < input.in > log.calc_w3d
-    mpirun -np "$mpi_np" ./calc_j3d < input.in > log.calc_j3d
-    cp "$ROOT_DIR/RESPACK/util/transfer_analysis/tr.py" "$calc_dir/"
+    cat > input.in <<EOF
+&param_chiqw
+flg_cRPA=1
+/
+&param_wannier
+/
+&param_interpolation
+/
+&param_visualization
+/
+&param_calc_int
+/
+EOF
+    mpirun -np "$mpi_np" calc_chiqw < input.in > log.chiqw
+    mpirun -np "$mpi_np" calc_w3d < input.in > log.calc_w3d
+    mpirun -np "$mpi_np" calc_j3d < input.in > log.calc_j3d
+    cp "$ROOT_DIR/RESPACK/util/transfer_analysis/tr.py" ./
     cd "$ROOT_DIR/RESPACK/src/transfer_analysis/" || return 1
     make
     cp calc_tr "$calc_dir/"
