ad574e83cb4343bd7d7bb80c4338167566b2e5b9
George Tsatas <93tsatas@gmail.com>
2026-01-02 16:02:21 -0500

    Implement get_structure function for OPTIMADE

--- bofs1/run/structure.py
+++ bofs1/run/structure.py
@@ -5,7 +5,28 @@ import spglib
 from ase import Atoms
 from ase.io import read, write
 import bofs1
+from pymatgen.ext.optimade import OptimadeRester
 
+def get_structure(source, material_id):
+    """
+    Download a structure from a specific OPTIMADE provider, save it 
+    to the current working directory, return the path.
+    source : str
+    	The database alias (ex: 'mp', 'cod', 'oqmd', 'aflow').
+    material_id : str
+    	The ID of the material (e.g., 'mp-149' or '1010068').
+    Returns 
+    structure_path : str
+    	Path to the saved structure.
+    """
+    with OptimadeRester(source) as optimade:
+        print(f"Querying {source.upper()} for {material_id}...")
+        results = optimade.get_structures(filter=f'id="{material_id}"')
+        structure_path = os.path.abspath(f"{material_id}.cif")
+        results[material_id].to(filename=structure_path)
+        print(f"{material_id} saved as: {structure_path}")
+        
+    return structure_path
 
 def normalize_structure(structure_path, relax_config):
     """
@@ -40,3 +61,5 @@ def normalize_structure(structure_path, relax_config):
     print(f"Detected space group after vc-relax: {spacegroup} ({number})")
 
     return str(serial_path)
+
+
