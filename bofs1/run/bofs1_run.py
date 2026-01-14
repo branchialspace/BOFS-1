6b4b7591a66f79d7a6935d91165f712e8171cc5b
George Tsatas <93tsatas@gmail.com>
2026-01-14 16:14:11 -0500

    force_symmorphic in final nscf for downstream yambo

--- bofs1/run/bofs1_run.py
+++ bofs1/run/bofs1_run.py
@@ -65,6 +65,7 @@ def bofs1_test(*args):
     bofs1.pwx(relaxed_path, scf_config)
     # QE NSCF+U+V
     nscf_config = copy.deepcopy(bofs1.pwx_nscf_config)
+    nscf_config['system']['force_symmorphic'] = True
     bofs1.pwx(relaxed_path, nscf_config)
     # Wannier90 preprocess
     pwo = f'{name}_nscf.pwo'
@@ -84,5 +85,6 @@ def bofs1_test(*args):
 if __name__ == '__main__':
     workflows = {name: func for name, func in globals().items() if callable(func) and not name.startswith('_')}
     workflows[sys.argv[1]](*sys.argv[2:])
+
 
 
