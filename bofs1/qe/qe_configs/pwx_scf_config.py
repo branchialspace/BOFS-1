0c111a3eac9052b1e1a29e5546b802b5204b351f
George Tsatas <93tsatas@gmail.com>
2026-01-14 16:56:28 -0500

    Update pwx_scf_config.py

--- bofs1/qe/qe_configs/pwx_scf_config.py
+++ bofs1/qe/qe_configs/pwx_scf_config.py
@@ -34,7 +34,7 @@ pwx_scf_config = {
         'ibrav': 0,
         'occupations': 'smearing',
         'smearing': 'fermi-dirac',
-        'degauss': 0.01,
+        'degauss': 0.002, # 0.002 bulk Bi, 0.01 Bi-MOF
         'noncolin': True,
         'lspinorb': True
     },
