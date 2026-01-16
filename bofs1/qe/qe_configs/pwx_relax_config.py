3e1c0849c94c9ec3930d861b9d242d7169a74994
George Tsatas <93tsatas@gmail.com>
2026-01-16 17:05:19 -0500

    raise bands slightly

--- bofs1/qe/qe_configs/pwx_relax_config.py
+++ bofs1/qe/qe_configs/pwx_relax_config.py
@@ -12,7 +12,7 @@ pwx_relax_config = {
     'kpts_k_minimum': 6,
     'kpts_k_spacing': 0.08,
     'kpts_shift': (0,0,0),
-    'nbnd_scalar': 1.2,
+    'nbnd_scalar': 1.3,
     'initial_u_value': "off",
     "magnetization": {
         "d_3d": 0.6,
