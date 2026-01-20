e02ab3a8bafcb25dda263cb3f4669ad52a745f3e
George Tsatas <93tsatas@gmail.com>
2026-01-20 16:55:35 -0500

    typo

--- bofs1/run/bofs1_run.py
+++ bofs1/run/bofs1_run.py
@@ -104,7 +104,7 @@ def bofs1_U(*args):
     pwi = f'{name}_nscf_ibz.pwi'
     w90_config = copy.deepcopy(bofs1.mlwf_config)
     bofs1.w90_win(pwo, pwi, w90_config, nokpts=True) # No k-points in .win for w2r .win reference
-    subprocess.run(f'bash ./bofs1/respack/respack_run.sh wan2respack_pre ./{name}/{name}.save {name} {pwi} {name}_nscf_ibz.win.win ./wan2respack_work', shell=True, check=True)
+    subprocess.run(f'bash ./bofs1/respack/respack_run.sh wan2respack_pre ./{name}/{name}.save {name} {pwi} {name}_nscf_ibz.win ./wan2respack_work', shell=True, check=True)
     subprocess.run(f'cp {name}.win {name}_ibz.win', shell=True, check=True)
     # QE NSCF W2R
     subprocess.run(f'mpirun -n {np} ./qe-7.5/bin/pw.x -nk 1 < {name}_nscf_w2r.in > {name}_nscf_w2r.out', shell=True, check=True)
@@ -125,6 +125,7 @@ def bofs1_U(*args):
 if __name__ == '__main__':
     workflows = {name: func for name, func in globals().items() if callable(func) and not name.startswith('_')}
     workflows[sys.argv[1]](*sys.argv[2:])
+
 
 
 
