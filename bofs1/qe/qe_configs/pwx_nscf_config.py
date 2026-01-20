c83881870410cf56bf3905348999d2706a87b96b
George Tsatas <93tsatas@gmail.com>
2026-01-20 14:57:57 -0500

    'kpts_eq_thr': 1.2,

--- bofs1/qe/qe_configs/pwx_nscf_config.py
+++ bofs1/qe/qe_configs/pwx_nscf_config.py
@@ -12,6 +12,7 @@ pwx_nscf_config = {
     'kpts_method': "kmeshpl",
     'kpts_k_minimum': 6,
     'kpts_k_spacing': 0.05,
+    'kpts_eq_thr': 1.2,
     'kpts_shift': (0,0,0),
     'nbnd_scalar': 2,
     'initial_u_value': "off",
