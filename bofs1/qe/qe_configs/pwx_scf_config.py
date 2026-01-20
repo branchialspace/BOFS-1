677f1c4f2bc061a933f59d56de0a67e213cc255c
George Tsatas <93tsatas@gmail.com>
2026-01-20 14:57:35 -0500

    'kpts_eq_thr': 1.2,

--- bofs1/qe/qe_configs/pwx_scf_config.py
+++ bofs1/qe/qe_configs/pwx_scf_config.py
@@ -11,6 +11,7 @@ pwx_scf_config = {
     'rho_scalar': 6,
     'kpts_k_minimum': 6,
     'kpts_k_spacing': 0.05,
+    'kpts_eq_thr': 1.2,
     'kpts_shift': (0,0,0),
     'nbnd_scalar': 1.3,
     'initial_u_value': "off",
